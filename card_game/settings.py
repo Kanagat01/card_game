4a37af9ae997f396d84aabb5fdb59b404fdb7bae
Kanagat01 <skanagat101@gmail.com>
2023-10-04 14:38:05 +0600

    add a domen to server_name nginx and installed_apps

--- card_game/settings.py
+++ card_game/settings.py
@@ -18,7 +18,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
+                 'cc86905604b2.vps.myjino.ru', 'game.ataman-club.ru']
 
 
 # Application definition
